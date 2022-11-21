95a6c99ab09dfc3fbf172f68a59122e397495bc6
Jheniffer <jheniffer.fernandesm@gmail.com>
2022-11-21 11:22:28 -0300

    exercicios aula 03

---
+++ Condicionais/src/Emprestimo.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Emprestimo {
+    public static void main(String[] args) {
+
+        int idade, parcela, renda, emprestimo, limite, valor;
+
+        Scanner info = new Scanner(System.in);
+
+        System.out.println("Informe sua idade ");
+        idade = info.nextInt();
+
+        System.out.println("Informe o numero de parcelas");
+        parcela = info.nextInt();
+
+        System.out.println("Qual a sua renda?");
+        renda = info.nextInt();
+
+        System.out.println("Qual o valor de empréstimo desejado?");
+        emprestimo = info.nextInt();
+
+        System.out.println("A pessoa que está solicitando o empréstimo tem " + idade + " anos de idade e " +
+                "tem o salário de " + renda);
+
+        System.out.println("O valor solicitado é de " + emprestimo +
+                " para ser pago em " + parcela + " parecelas");
+
+        valor = emprestimo / parcela;
+        limite = renda - (30 / 100);
+
+        info.close();
+
+        if (idade >= 18 && idade <= 65) {
+            System.out.println("O empréstimo foi reprovado");
+        } else if (parcela >= 3 && parcela <= 24) {
+            System.out.println("O empréstimo foi reprovado");
+        } else if (valor <= limite) {
+            System.out.println("O empréstimo foi aprovado");
+        } else {
+            System.out.println("O empréstimo foi reprovado");
+
+        }
+    }
+
+}
---
+++ LacodeRepeticao/src/Escada.java
@@ -0,0 +1,10 @@
+public class Escada {
+    public static void main(String[] args) {
+
+        String escada = "" ;
+        for (int i = 1; i <=5 ; i++) {
+            System.out.println(escada += "#");
+            
+        }
+    }
+}
